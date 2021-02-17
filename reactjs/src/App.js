ffac513bead5603a7254f210f707947be6482236
Abdullah Saif <58792400+abdullahsaif@users.noreply.github.com>
2021-02-18 02:51:19 +0500

    Update App.js

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World form us</h1>
+        <h1>Hello World From US</h1>
       </header>
     </div>
   );
